21c828c6df39fcda82f22de7d17e00a0616866ea
ladybacon <ladybaconshopping@gmail.com>
2023-01-17 13:48:33 +0100

    m

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\VacationController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/vacation', [VacationController::class, 'index']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
