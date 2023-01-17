928ca934d2b350f9ca25d2703697de8b903fa7e1
Rico van Zelst <ricovzelst@pm.me>
2023-01-17 11:01:00 +0100

    add; few migrations tested working

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use App\Models\Order;
+use App\Models\Employee;
+use App\Models\Function_db;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -18,18 +19,16 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
+
+        Function_db::factory(10)->create();
+
+        Employee::factory(10)->create();
         User::factory()->create([
+            'employee_id' => '1',
             'name' => 'TestAdmin',
             'email' => 'test@admin.com',
-            'phone_number' => '0612345678',
             'password' => bcrypt('password'), // password
-            'role' => 'owner',
-            'first_name' => 'Test',
-            'last_name' => 'Admin',
-            'address' => 'H20 Purmerend',
-            'date_of_birth' => '1990-01-01',
         ]);
-
-        Order::factory()->count(10)->create();
+        User::factory(10)->create();
     }
 }
