30e82e4ea201b0e9f49d00cc1f14dcc8fb28d7e1
Mingus Wankum <16616@student.horizoncollege.nl>
2023-01-18 10:49:33 +0100

    Bestellingen

--- resources/views/dash/orders.blade.php
+++ resources/views/dash/orders.blade.php
@@ -9,26 +9,29 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
-                <div class="p-6 text-gray-900">
-               
                 <table class="shadow-md rounded-lg overflow-hidden">
   <thead class="bg-gray-300">
     <tr class="text-gray-700">
       <th class="px-4 py-2">ID</th>
-      <th class="px-4 py-2">Product Name</th>
-      <th class="px-4 py-2">Made At</th>
+      <th class="px-4 py-2">Naam</th>
+      <th class="px-4 py-2">Email</th>
+      <th class="px-4 py-2">Telefoonnummer</th>
+      <th class="px-4 py-2">Prijs</th>     
+      <th class="px-4 py-2">Ophaal Datum</th>
       <th class="px-4 py-2">Status</th>
+
     </tr>
   </thead>
   <tbody>
-    @foreach ($products as $product)
+    @foreach ($orders as $order)
     <tr class="text-gray-700">
-      <td class="border-t px-4 py-2">{{ $product->id }}</td>
-      <td class="border-t px-4 py-2">{{ $product->product_name }}</td>
-      <td class="border-t px-4 py-2">{{ $product->made_at }}</td>
-      <td class="border-t px-4 py-2">{{ $product->status }}</td>
+      <td class="border-t px-4 py-2">{{ $order->id }}</td>
+      <td class="border-t px-4 py-2">{{ $order->name }}</td>
+      <td class="border-t px-4 py-2">{{ $order->email }}</td>
+      <td class="border-t px-4 py-2">{{ $order->phone }}</td>
+      <td class="border-t px-4 py-2">{{ $order->total_price }}</td>
+      <td class="border-t px-4 py-2">{{ $order->pickup_date }}</td>
+      <td class="border-t px-4 py-2">{{ $order->status }}</td>
     </tr>
     @endforeach
   </tbody>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,9 +19,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/bestellingen', [DashboardController::class, 'bestellingen'])->middleware(['auth', 'verified'])->name('bestellingen');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -28,4 +32,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
