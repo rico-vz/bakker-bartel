415af4347a2b29f975cbc12c652b031bbcb79f18
Dylan Kuiper <83163353+Dylan-Kuiper@users.noreply.github.com>
2023-01-18 09:40:54 +0100

    welkom en about pagina (#15)

---
+++ about.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Bakker Bartel - Over Ons</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css">
+  </head>
+  <body class="bg-gray-200">
+    <nav class="bg-orange-200 p-6 shadow">
+    <div class="flex items-center justify-between">
+        <div class="flex items-center">
+        <a href="/" class="text-lg font-medium">Bakker Bartel</a>
+        <div class="ml-4">
+            <a href="/about" class="text-sm font-medium ml-4">About</a>
+            <a href="/aanvraag" class="text-sm font-medium ml-4">aanvraag</a>
+        </div>
+        </div>
+    </div>
+    </nav>
+    <div class="container bg-white mx-auto p-4 mt-4">
+      <h1 class="text-2xl font-medium">Welkom bij Bakker Bartel</h1>
+      <p class="text-lg">Wij zijn een familiebedrijf dat al generaties lang brood en gebak produceert. Onze bakkerij is gevestigd in het hart van de stad en we zijn trots op de traditionele recepten en technieken die we gebruiken om onze producten te maken.</p>
+      <p class="text-lg">Onze bakkers werken elke dag hard om verse producten te produceren, van knapperige broodjes tot heerlijke taarten en gebak. We gebruiken alleen de beste ingrediënten en we zijn trots op onze ambachtelijke aanpak.</p>
+      <p class="text-lg">We bieden ook een scala aan speciale diëten, zoals glutenvrij en suikervrij, zodat iedereen kan genieten van onze producten.</p>
+      <p class="text-lg">Dank u voor uw interesse in Bakker Bartel. We hopen u snel te zien in onze bakkerij!</p>
+    </div>
+  </body>
+</html>
---
+++ welcome.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Baker Bartel</title>
+  <link href="https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css" rel="stylesheet">
+</head>
+<body class="bg-gray-200 text-black">
+<nav class="bg-orange-200 p-6 shadow">
+  <div class="flex items-center justify-between">
+    <div class="flex items-center">
+      <a href="/" class="text-lg font-medium">Bakker Bartel</a>
+      <div class="ml-4">
+        <a href="/about" class="text-sm font-medium ml-4">About</a>
+        <a href="/aanvraag" class="text-sm font-medium ml-4">aanvraag</a>
+      </div>
+    </div>
+  </div>
+</nav>
+
+  <div class="container mx-auto p-4 flex flex-col items-center">
+    <h1 class="text-4xl font-medium mb-8">Welkom bij Bakker Bartel</h1>  
+        <h2 class="text-2xl font-medium mt-6">Ons assortiment van muffins tot broden. </h2>
+      <div class="flex flex-wrap justify-center">
+        <div class="bg-white p-4 mt-4 w-1/3">
+          <img src="https://lh3.googleusercontent.com/crpuZxHo9EFJQFt1K4pQfss9u2YVtUZ1CnsDzjJZefl-M-bIqAiSkkXVJU0Vc21Kn09yv1IYBmEK7MHzs50CHdMFHh4RyzKx-0Usikgj-mtpY8SX7dkZ" alt="muffins">
+        </div>
+          <div class="bg-white p-4 mt-4 w-1/3">
+            <img src="https://cdn.shopify.com/s/files/1/1786/0595/products/Pastry-GermanCroissant_9209fe40-2ed5-4e6f-8d88-112c32acfee7_512x512.jpg?v=1645635736" alt="croissant">
+        </div>
+        <div class="bg-white p-4 mt-4 w-1/3">
+          <img src="https://i.pinimg.com/originals/05/21/d8/0521d8c71dbb457ce73a164ae57c3bdc.jpg" alt="baguette">
+      </div>
+      </div>
+  </div>
