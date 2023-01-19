0cbda837d0cdc9a96df6f4058fe62937c7430c51
Rico van Zelst <ricovzelst@pm.me>
2023-01-19 14:36:45 +0100

    Restore resources/views/dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,43 +1,24 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="text-xl font-semibold leading-tight text-gray-800">
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
-                <div class="p-6 text-gray-900">
-               
-                <table class="shadow-md rounded-lg overflow-hidden">
-  <thead class="bg-gray-300">
-    <tr class="text-gray-700">
-      <th class="px-4 py-2">ID</th>
-      <th class="px-4 py-2">Product Name</th>
-      <th class="px-4 py-2">Made At</th>
-      <th class="px-4 py-2">Status</th>
-    </tr>
-  </thead>
-  <tbody>
-    @foreach ($products as $product)
-    <tr class="text-gray-700">
-      <td class="border-t px-4 py-2">{{ $product->id }}</td>
-      <td class="border-t px-4 py-2">{{ $product->product_name }}</td>
-      <td class="border-t px-4 py-2">{{ $product->made_at }}</td>
-      <td class="border-t px-4 py-2">{{ $product->status }}</td>
-    </tr>
-    @endforeach
-  </tbody>
-</table>
-
+                    {{-- Goodmorning, goodafternoon or goodevening depending on the current time --}}
+                    <h1 class="text-3xl font-bold text-center">
+                        {{ __('Goede' . (date('H') < 12 ? 'morgen' : (date('H') < 18 ? 'middag' : 'avond')) . ', ' . Auth::user()->name) . ' [' . Auth::user()->employee->function->name . ']' }}
+                    </h1>
+                    {{-- Het is vandaag <todays date> --}}
+                    <h2 class="text-xl font-semibold text-center">
+                        {{ __('Het is vandaag ' . date('d-m-Y')) }}
+                    </h2>
 
                 </div>
-
             </div>
         </div>
-    </div>
 </x-app-layout>
